bc9379ef81eb716522006bf3a46fa18ba5afb1a8
KliminchenkoSergey <44263760+KliminchenkoSergey@users.noreply.github.com>
2024-12-19 14:46:02 +0300

    Update obrabotka.php

--- obrabotka.php
+++ obrabotka.php
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit;
     }
 
-    $conn = mysqli_connect("localhost", "bitrix24", "Xzup2jWB", "reminderInTG");
+    $conn = mysqli_connect("localhost", "bitrix24", "####", "reminderInTG");
     if (!$conn) {
         die("Ошибка подготовки запроса: " . mysqli_error($conn));
     }
